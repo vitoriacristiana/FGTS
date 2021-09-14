a5e0d31e118443679704e21655027ebbf457d335
Vitória Cristiana <vitoriacristiana290@gmail.com>
2021-09-14 18:28:48 -0300

    exercício concluido

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace FGTS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.ReadLine();
+            double salário;
+            decimal fgts;
+
+            Console.WriteLine("Bem vindo ao cálculo de IMC! /n");
+            Console.Write("Salário(R$)...");
+            salário=Convert.ToDouble(Console.ReadLine());
+            
+            fgts=Convert.ToDecimal(salário *0.08);
+            Console.WriteLine($"/n FGTS: {fgts.ToString("c")}");
+
+
+
+
+
+
+            
+
+        }
+    }
+}
